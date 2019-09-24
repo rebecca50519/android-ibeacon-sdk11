2264e19ab0abf4ea644bbc1b4e441157b3a6deac
rebecca50519 <rebecca50519@gmail.com>
2019-09-24 12:21:02 +0800

    Merge branch 'master' of https://github.com/firebase-indoor-beacon/android-ibeacon-sdk
    # Conflicts:
    #	androidibeaconservice/build/outputs/logs/manifest-merger-debug-report.txt

--- app/src/main/java/com/example/ibeacon/MainActivity.java
+++ app/src/main/java/com/example/ibeacon/MainActivity.java
@@ -1,19 +1,71 @@
 package com.example.ibeacon;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
-import com.radiusnetworks.ibeacon.IBeaconManager;
-public class MainActivity extends AppCompatActivity {
 
+import com.radiusnetworks.ibeacon.IBeaconManager;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer=findViewById(R.id.drawer_layout);
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new MessageFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_beacon);
+        }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_beacon:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                break;
+            case R.id.nav_Popular:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PopularFragment()).commit();
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
     public void onMonitoringClicked(View view) {
         Intent myIntent = new Intent(this, MonitoringActivity.class);
         this.startActivity(myIntent);
